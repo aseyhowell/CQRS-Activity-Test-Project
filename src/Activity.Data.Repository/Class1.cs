805800539a6669dc0cc75265e5596e41f4d7fa03
Andrew Seymour-Howell <A.Seymour-Howell@traceone.com>
2020-02-11 17:06:20 +0000

    Initial checking (playing around)

---
+++ src/Activity.Data.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Activity.Data.Repository
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Activity.Services.ActivityWebApi/Controllers/EventStoreTestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventStore.ClientAPI;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Activity.Services.ActivityWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventStoreTestController : ControllerBase
+    {
+        public IActionResult GetEvents()
+        {
+            var conn = EventStoreConnection.Create(new Uri("tcp://admin:changeit@127.0.0.1:1113"),
+                "InputFromFileConsoleApp");
+             conn.ConnectAsync().Wait();
+            conn.AppendToStreamAsync("TestStreamName", ExpectedVersion.Any,new EventData(Guid.NewGuid(), "TestEvent",true,null,null)).Wait();
+            Console.WriteLine($"Published test events to eventstore");
+
+            return Ok();
+        }
+    } 
+    
+}
